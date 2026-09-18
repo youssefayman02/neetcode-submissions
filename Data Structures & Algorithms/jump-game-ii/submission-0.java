c908b957dec67fa4f8ee54284842f565d9fddcad
Youssef Ayman <102010875+youssefayman02@users.noreply.github.com>
2026-09-18 13:47:58 +0300

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int l = 0, r = 0, ans = 0;
+
+        while (r < nums.length - 1) {
+            int maxJump = 0;
+            for (int i = l; i <= r; i++) {
+                maxJump = Math.max(maxJump, i + nums[i]);
+            }
+            l = r + 1;
+            r = maxJump;
+            ans++;
+        }
+
+        return ans;
+    }
+}
