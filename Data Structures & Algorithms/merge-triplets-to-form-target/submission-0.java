ef973b776fb3aa35857973152806d0b346bc1e21
Youssef Ayman <102010875+youssefayman02@users.noreply.github.com>
2026-09-22 19:53:57 +0300

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int[] t : triplets) {
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) {
+                continue;
+            }
+            for (int i = 0; i < t.length; i++) {
+                if (t[i] == target[i]) set.add(i);
+            }
+        }
+
+        return set.size() == 3;
+    }
+}
